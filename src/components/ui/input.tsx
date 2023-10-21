49010ba34ee270cd028c3fcf663509cbc0fda33a
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2023-10-21 22:46:53 +0200

    style: Fix font for iphones

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "placeholder:text-muted-foreground flex h-10 w-full rounded-md border border-gray-300 bg-gray-500 px-3 py-1 text-sm shadow-sm transition-colors duration-300 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1  focus-visible:ring-brand-light disabled:cursor-not-allowed disabled:opacity-50",
+          "placeholder:text-muted-foreground flex h-10 w-full rounded-md border border-gray-300 bg-gray-500 px-3 py-1 text-base shadow-sm transition-colors duration-300 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none  focus-visible:ring-1 focus-visible:ring-brand-light disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,4 +66,7 @@ export default {
     },
   },
   plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
 }
